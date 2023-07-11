43e82aefd86bf2f68893db990e5ccbc5d107fb1e
isthifa <4pa19cs404@pace.edu.in>
2023-07-11 14:59:17 +0530

    basic math

---
+++ task3.js
@@ -0,0 +1,23 @@
+//Math 
+let x=3.14;
+let f=3.44;
+let y=Math.round(x);
+console.log(y);
+let z=Math.ceil(x);
+console.log(z);
+let a=Math.floor(x);
+console.log(a);
+let b=Math.pow(2,3);
+console.log(b);
+let c=Math.sqrt(9);
+console.log(c);
+let d=Math.abs(-3.14);
+console.log(d);
+let max=Math.max(1,2,3,4,5,6,7,8,9,10);
+console.log(max);
+let min=Math.min(1,2,3,4,5,6,7,8,9,10);
+console.log(min);
+let e=Math.random();
+console.log(e);
+
+
