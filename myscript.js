742e3fb22e9372a0099e2371f685405ac1ed83ba
isthifa <4pa19cs404@pace.edu.in>
2023-07-11 14:30:22 +0530

    initial commit

---
+++ myscript.js
@@ -0,0 +1,5 @@
+let username;
+document.getElementById("btn").onclick=function(){
+    username=document.getElementById("textdata").value;
+    document.getElementById("data").innerHTML=username;
+}
