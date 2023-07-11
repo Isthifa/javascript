20f3e59e2086dcbd650269a8376388482faf2a68
isthifa <4pa19cs404@pace.edu.in>
2023-07-11 15:59:08 +0530

    string opertions

---
+++ task4.js
@@ -0,0 +1,58 @@
+//usefull String methods
+//1. length
+//2. indexOf
+//3. lastIndexOf
+//4. search
+//5. slice
+//6. substring
+//7. substr
+//8. replace
+//9. toUpperCase
+//10. toLowerCase
+//11. concat
+//12. trim
+//13. charAt
+//14. charCodeAt
+//15. split
+//16. includes
+//17. repeat
+//18. match
+//19. matchAll
+//20. startsWith
+//21. endsWith
+//22. localeCompare
+//23. toString
+//24. valueOf
+//25. anchor
+//26. big
+//27. blink
+//28. bold
+//29. fixed
+//30. fontcolor
+//31. fontsize
+//32. italics
+//33. link
+let a="isthifa";
+console.log(a.length)
+console.log(a.indexOf("t"))
+console.log(a.lastIndexOf("a"))
+console.log(a.search("h"))
+console.log(a.slice(2,5))
+console.log(a.substring(2,5))
+console.log(a.substr(2,5))
+console.log(a.replace("i","I"))
+console.log(a.toUpperCase())
+console.log(a.toLowerCase())
+console.log(a.concat(" is "))
+console.log(a.trim())
+console.log(a.charAt(2))
+console.log(a.charCodeAt(2))
+console.log(a.split("t"))
+console.log(a.includes("t"))
+console.log(a.repeat(2))
+console.log(a.match("t"))
+console.log(a.matchAll("t"))
+console.log(a.startsWith("i"))
+console.log(a.endsWith("a"))
+console.log(a.localeCompare("isthifa"))
+console.log(a.toString())
