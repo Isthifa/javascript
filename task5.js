9be59729ddfef1f596ac3f135a275800dbf1d755
isthifa <4pa19cs404@pace.edu.in>
2023-07-11 16:17:32 +0530

    if else

---
+++ task5.js
@@ -0,0 +1,10 @@
+let age=window.prompt("Enter your age");
+if(age>=18){
+    console.log("You are eligible to vote")
+}
+else if(age==17){
+    console.log("You are not eligible to vote")
+}
+else{
+    console.log("Not eligible to vote")
+}
