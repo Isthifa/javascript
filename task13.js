ee29220358049db0dd2a8e700bb3323686239177
isthifa <4pa19cs404@pace.edu.in>
2023-07-12 12:36:42 +0530

    this keyword

--- task13.js
+++ task13.js
@@ -2,6 +2,7 @@
 //object is a non-primitive data type in javascript.
 //object is a reference type.
 //object is a mutable data type.
+//object is group of properties and methods.
 
 const car = {
     name: "BMW",
@@ -28,3 +29,26 @@ car.drive();
 console.log(car["name"]);
 console.log(car["model"]);
 console.log(car["color"]);
+
+//this keyword in javascript is used to refer to the current object.
+//this keyword is used to access the properties and methods of the current object.
+
+const car1 = {
+    name: "BMW",
+    model: "X5",
+    color: "Black",
+
+    start: ()=> {
+        console.log(`${this.name} started`);
+    },
+    stop:  ()=> {
+        console.log(`${this.name} stopped`);
+    },
+    drive:  ()=> {
+        console.log(`${this.name} driving`);;
+    }
+}
+console.log(car1);
+car1.start();
+car1.stop();
+car1.drive();
